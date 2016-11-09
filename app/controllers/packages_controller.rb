fcb59aca044f3182ee789d8c90ba038f4636d3a5
Beylul <bkbreab8@gmail.com>
2016-11-09 12:03:38 -0800

    formatted USPS data

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -13,9 +13,11 @@ class PackagesController < ApplicationController
   SIZE_HEIGHT = 15
   SIZE_LENGTH = 4.5
   ORIGIN = ActiveShipping::Location.new(country: O_COUNTRY, state: 'WA', city: 'Seattle', postal_code: '98102')
-  LOGIN = ENV['ACTIVESHIPPING_UPS_LOGIN']
+  UPS_LOGIN = ENV['ACTIVESHIPPING_UPS_LOGIN']
   PASSWORD = ENV['ACTIVESHIPPING_UPS_PASSWORD']
   KEY = ENV['ACTIVESHIPPING_UPS_KEY']
+  USPS_LOGIN = ENV['ACTIVESHIPPING_USPS_LOGIN']
+
 
 
   def find_rate
@@ -27,31 +29,36 @@ class PackagesController < ApplicationController
     # city: params[:city], postal_code: params[:postal_code].to_i)
     destination = ActiveShipping::Location.new(country: D_COUNTRY, state: 'WA', city: 'Seattle', postal_code: '98118')
 
-    # usps_rates = get_usps_rates(origin, destination, packages)
+    usps_rates = get_usps_rates(ORIGIN, destination, packages)
     ups_rates = get_usp_rates(ORIGIN, destination, packages)
 
-    # response = {"usps" => usps_rates, "ups" => ups_rates}
-    response = {"ups" => ups_rates}
+    response = {"ups" => ups_rates, "usps" => usps_rates}
 
     render json: response
   end
 
-  # def get_usps_rates(origin, destination, packages)
-  #   usps = ActiveShipping::USPS.new(login: ENV["ACTIVESHIPPING_USPS_LOGIN"])
-  #   response = usps.find_rates(origin, destination, packages)
-  #
-  #   usps_rates = {}
-  #   usps_rates = response.rates.sort_by(&:price).each do |rate|
-  #     usps_rates[rate.service_name] =  rate.price
-  #   end
-  #   return usps_rates
-  # end
+  def get_usps_rates(origin, destination, packages)
+    usps = ActiveShipping::USPS.new(login: USPS_LOGIN)
+    response = usps.find_rates(ORIGIN, destination, packages)
+    rates =[]
+    response.rates.each do |rate|
+      usps_rate = {}
+      usps_rate[:name] = rate.service_name
+      usps_rate[:cost] = ((rate.total_price.to_f)/100)
+      if !(rate.delivery_range.empty?)
+        usps_rate[:delivery] = rate.delivery_range
+      end
+      rates << usps_rate
+    end
+    return rates
+  end
+
 
   def get_usp_rates(origin, destination, packages)
-    ups = ActiveShipping::UPS.new(login: LOGIN,
+    ups = ActiveShipping::UPS.new(login: UPS_LOGIN,
     password: PASSWORD, key: KEY)
     response = ups.find_rates(ORIGIN, destination, packages)
-    ups =[]
+    @package_rates =[]
     response.rates.each do |rate|
       ups_rate = {}
       ups_rate[:name] = rate.service_name
@@ -59,8 +66,8 @@ class PackagesController < ApplicationController
       if !(rate.delivery_range.empty?)
         ups_rate[:delivery] = rate.delivery_range
       end
-      ups << ups_rate
+      @package_rates << ups_rate
     end
-    return ups
+    return @package_rates
   end
 end
