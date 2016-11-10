46e0d741431fe32018b3ea6d5e5f656d104550b3
Madeleine Johnson <mjohnson3038@gmail.com>
2016-11-10 09:37:55 -0800

    Working params and first test completed

--- test/controllers/packages_controller_test.rb
+++ test/controllers/packages_controller_test.rb
@@ -9,18 +9,17 @@ class PackagesControllerTest < ActionController::TestCase
   end
 
   test "when #find_rate invoked package rate is returned" do
-    # ORIGIN = country: "US", state: 'WA', city: 'Seattle', postal_code: '98102'
-    # DESTINATION = state: "WA", city: Seattle: postal_code: "98102"
-    package_data = {
-      "orgin" => {"state" => "WA", "city" => "Seattle", "postal_code" => "98112"},
+    params = {
       "weight" => 10,
-      "destination" => {"state" => "WA", "city" => "Seattle", "postal_code" => "98102"}
-      }
-      post :find_rate, {"package": package_data}
+      "state" => "WA",
+      "city" => "Seattle",
+       "zip" => "98102"
+    }
+    post :find_rate, params
 
-      assert_match 'application/json', response.header['Content-Type']
-      body = JSON.parse(response.body)
+    assert_match 'application/json', response.header['Content-Type']
+    body = JSON.parse(response.body)
 
-      assert_instance_of Hash, body
-    end
+    assert_instance_of Hash, body
   end
+end
